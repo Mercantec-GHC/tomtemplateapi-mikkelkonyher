fc3e44954b3d1e92e68b1976f4f6b438423829be
Mikkel konyher <mikkelkonyher@gmail.com>
2024-08-07 13:19:43 +0200

    test

--- GaspricesAPI/obj/Debug/net8.0/GaspricesAPI.AssemblyInfo.cs
+++ GaspricesAPI/obj/Debug/net8.0/GaspricesAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GaspricesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5e020371f48b86b15df6d734770877a5c4bbb1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GaspricesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GaspricesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
