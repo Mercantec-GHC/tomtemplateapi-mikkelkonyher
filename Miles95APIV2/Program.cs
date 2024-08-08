55040189d938908ec7159627abc5f439925a8ae4
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2024-08-08 15:58:52 +0200

    API 8.8 15.58

--- Miles95APIV2/Program.cs
+++ Miles95APIV2/Program.cs
@@ -6,6 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Registrerer en HttpClient service med en "scoped lifetime"(En ny instans af servicen bliver kaldt for hver request)
+//  (lamba expression "=>") tager sp(IServiceProvider) som et input og returnerer en ny instance af HttpClient
+// BaseAddress for den nye HttpClient instans bliver sat til den base URL, hvorfra applikationen er hostet, nemlig builder.HostEnvironment.BaseAddress.
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
--- Miles95APIV2/obj/Debug/net8.0/Miles95APIV2.AssemblyInfo.cs
+++ Miles95APIV2/obj/Debug/net8.0/Miles95APIV2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Miles95APIV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15c7c195d1bbd847e4e9f554f5ee563cdd87877")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d958a23abd0186905ed7b8d699fa1e8aafb72001")]
 [assembly: System.Reflection.AssemblyProductAttribute("Miles95APIV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Miles95APIV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
