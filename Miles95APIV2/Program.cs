ddc22e325bb2fc3f21a0da5c4808578271394537
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2024-08-09 11:40:44 +0200

    API Update 9.8 11.40

--- Miles95APIV2/Program.cs
+++ Miles95APIV2/Program.cs
@@ -6,10 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Registrerer en HttpClient service med en "scoped lifetime"(En ny instans af servicen bliver kaldt for hver request)
+// Registrerer en HttpClient service med en "scoped lifetime"(En ny instans af servicen bliver oprettet for hver request)
 //  (lamba expression "=>") tager sp(IServiceProvider) som et input og returnerer en ny instance af HttpClient
-// BaseAddress for den nye HttpClient instans bliver sat til den base URL, hvorfra applikationen er hostet, nemlig builder.HostEnvironment.BaseAddress.
+// BaseAddress for den nye HttpClient instans bliver sat til den faste base URL for API-kald
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://magsapi.onrender.com") });
 
 await builder.Build().RunAsync();
--- Miles95APIV2/obj/Debug/net8.0/Miles95APIV2.AssemblyInfo.cs
+++ Miles95APIV2/obj/Debug/net8.0/Miles95APIV2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Miles95APIV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55040189d938908ec7159627abc5f439925a8ae4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90da703a3134ac7ab649ce686b9b34e91a81a35a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Miles95APIV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Miles95APIV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
